c74f1e7739faaaf865f52b4d882f99e9192d5e89
Serj302 <MarS302@mail.ru>
2020-04-19 23:25:00 +0300

    Пример по Мат операциям

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,6 +5,32 @@ public class MyFirstProgram {
     public static void main(String[] args) {
 
         System.out.println("Hello, World!");
+
+        System.out.println(2 + 2); // sout + enter
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(2 - 2);
+
+        //Дроби
+        System.out.println(1 / 2);   // 0 - т.к. int - будет 0, округление целого
+        System.out.println(1.0 / 2); // 0.5
+        System.out.println(1 / 2.0); // 0.5
+        System.out.println(2.0 / 2); // 1.0
+
+        // сложение строк
+        System.out.println("2" + "2"); // 22
+        System.out.println("2" + 2);  // 22 - число преобразуется в строку, в итоге строка + строка
+        System.out.println(2 + "2"); //22 - всё-равно число преобразуется в строку
+
+        System.out.println(2 + 2 * 2); // 6
+        System.out.println((2 + 2) * 2 ); // 8
+
+        System.out.println("2 + 2 = " + 2 + 2); // 2 + 2 = 22
+        System.out.println("2 + 2 = " + (2 + 2) ); // 2 + 2 = 4
+
+        
+
+
     }
 
 }
