dba2159fdae088fd7c5dce95029163d4e70108a9
Serj302 <MarS302@mail.ru>
2020-04-23 19:18:10 +0300

    Задание №2: Потренироваться использовать функции, классы, объекты и методы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,8 +4,9 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
-
+        //Объект p
+        Point p = new Point(1,1,5,3);
+           System.out.println("Расстояние между 2мя точками = " + p.distance ());
 
     }
 
