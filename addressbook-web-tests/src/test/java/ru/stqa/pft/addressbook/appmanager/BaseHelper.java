7834ab2c429c720402162a71955b82e07120e21b
Serj302 <MarS302@mail.ru>
2020-05-10 11:28:50 +0300

    Не менять совпадающее значение в поле формы_v3

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/BaseHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/BaseHelper.java
@@ -18,7 +18,7 @@ public class BaseHelper {
 
   protected void type(By locator, String text) {
     click(locator);
-    if (text != null){ // если передаем null - то ничего не делать, в противном выполнить код:
+    if (text != null){ // если передаем null - то ничего не делать, в противном - выполнить код:
       String existingText = wd.findElement(locator).getAttribute("value"); // получили значение с поля ввода
       // этот if необходим для оптимизации больших полей ввода, куда вводится длинный текст, от 1000-2000 и более символов
       if (! text.equals(existingText)){ // если не верно, что передаваемый текст совпадает уже с существующ текстом в форме, то выполнить код:
@@ -38,12 +38,12 @@ public class BaseHelper {
   }
 
   protected void select(By locator, String text) {
-    if (text != null)
+    if (text != null) //выполнять условие, только если передается какое-то значение, если null - ничего делать не нужно
     new Select(wd.findElement(locator)).selectByVisibleText(text);
   }
 
   protected void photo(By locator, String value) {
-    if (value != null)
+    if (value != null) //выполнять условие, только если передается какое-то значение, если null - ничего делать не нужно
     wd.findElement(locator).sendKeys(value);
   }
 }
