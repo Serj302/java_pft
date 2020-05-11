095ae81afd5d43b5d68e62f1330df13f73a0c1b8
Serj302 <MarS302@mail.ru>
2020-05-11 21:12:45 +0300

    Проверка на наличие контакта. Если контакта нет - создаёт_v2

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
@@ -20,39 +21,39 @@ public class ContactHelper extends BaseHelper{
 
   public void fillContactForm(ContactData contactData, boolean creation) {
 
-    type(name("firstname"), contactData.getFirstname());
+    type(By.name("firstname"), contactData.getFirstname());
 
-    type(name("middlename"), contactData.getMiddlename());
+    type(By.name("middlename"), contactData.getMiddlename());
 
-    type(name("lastname"), contactData.getLastname());
+    type(By.name("lastname"), contactData.getLastname());
 
-    type(name("nickname"), contactData.getNickname());
+    type(By.name("nickname"), contactData.getNickname());
 
-    photo(name("photo"), contactData.getPhoto());
+    photo(By.name("photo"), contactData.getPhoto());
 
-    type(name("title"), contactData.getTitle());
+    type(By.name("title"), contactData.getTitle());
 
-    type(name("company"), contactData.getCompany());
+    type(By.name("company"), contactData.getCompany());
 
-    type(name("address"), contactData.getAddress());
+    type(By.name("address"), contactData.getAddress());
 
-    type(name("home"), contactData.getTel_home());
+    type(By.name("home"), contactData.getTel_home());
 
-    type(name("mobile"), contactData.getTel_mobile());
+    type(By.name("mobile"), contactData.getTel_mobile());
 
-    type(name("work"), contactData.getTel_work());
+    type(By.name("work"), contactData.getTel_work());
 
-    type(name("fax"), contactData.getFax());
+    type(By.name("fax"), contactData.getFax());
 
-    type(name("email"), contactData.getEmail());
+    type(By.name("email"), contactData.getEmail());
 
-    type(name("homepage"), contactData.getHomepage());
+    type(By.name("homepage"), contactData.getHomepage());
 
-    select(name("bday"), contactData.getBday());
+    select(By.name("bday"), contactData.getBday());
 
-    select(name("bmonth"), contactData.getBmonth());
+    select(By.name("bmonth"), contactData.getBmonth());
 
-    type(name("byear"), contactData.getByear());
+    type(By.name("byear"), contactData.getByear());
 
     /* выбор элемента из выпадающего списка:
      *
@@ -147,20 +148,20 @@ public class ContactHelper extends BaseHelper{
 
     if (creation){
       //
-      new Select(wd.findElement(name("new_group"))).selectByVisibleText(contactData.getGroup());
+      new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroup());
     } else {
-      Assert.assertFalse(isElementPresent(name("new_group")));
+      Assert.assertFalse(isElementPresent(By.name("new_group")));
     }
 
-    type(name("address2"), contactData.getAddress2());
+    type(By.name("address2"), contactData.getAddress2());
 
-    type(name("phone2"), contactData.getPhone2());
+    type(By.name("phone2"), contactData.getPhone2());
 
-    type(name("notes"), contactData.getNotes());
+    type(By.name("notes"), contactData.getNotes());
   }
 
   public void selectContact() {
-    click(name("selected[]"));
+    click(By.name("selected[]"));
   }
 
   public void deletedContact() {
@@ -180,7 +181,7 @@ public class ContactHelper extends BaseHelper{
   }
 
   public boolean isThereAContac() {
-    return isElementPresent(name("selected[]"));
+    return isElementPresent(By.name("selected[]"));
   }
 
 }
