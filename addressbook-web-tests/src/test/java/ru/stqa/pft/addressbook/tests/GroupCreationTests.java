d370d0b02b7b0b0a85b86f4ad894c0b1a3df134e
Serj302 <MarS302@mail.ru>
2020-05-18 19:47:29 +0300

    4.9. Лямбда-выражения (анонимные функции)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -108,7 +109,7 @@ public class GroupCreationTests extends TestBase {
 // преобразование списка в множества
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 */
-/*---------------------TOTAL--------------------------------------------*/
+/*---------------------TOTAL--------------------------------------------
 
   @Test
   public void testGroupCreation() throws Exception {
@@ -129,12 +130,116 @@ public class GroupCreationTests extends TestBase {
     }
 
     group.setId(max);
-
     before.add(group);
-
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
   }
+
+---------------------------------------------------------------------*/
+/*
+  @Test
+  public void testGroupCreation() throws Exception {
+    app.getNavigationHelper().gotoGroupPage();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    GroupData group = new GroupData("test3", "test_test", "test_test_test");
+    app.getGroupHelper().createGroup(group);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+
+    Assert.assertEquals(after.size(), before.size() + 1);
+
+// ПОЛУЧЕНИЕ MAX ID
+
+    // I ВАРИАНТ - Цикл
+
+    int max = 0;
+
+    for (GroupData g : after){
+      if (g.getId() > max){
+        max = g.getId();
+      }
+    }
+
+
+    // II ВАРИАНТ - Comparator
+
+
+
+*  2.
+* реализовать сравниватель (byId)
+    Comparator<? super GroupData> byId = ... ;
+*  3. переменную byId - надо инициализировать, присвоить в нее какое-то конкретное значение
+
+    Comparator<? super GroupData> byId = new Comparator<GroupData>() {  // Comparator<? super GroupData> byId = сtrl+пробел
+* создаем объект типа Компаратор - это интерфейс без реализации, он только декларирует (объявляет, какие методы должны быть), но
+* реализацию этих методов - не содержит
+      @Override
+      public int compare(GroupData o1, GroupData o2) {  // о1 о2 - это два id, котор будут сравниваться
+        return Integer.compare(o1.getId(), o2.getId());   // метод для сравнения чисел
+      }
+    };
+
+*  1.
+* превратить список в поток (stream), у потока есть метод max, для вычисления max элемента
+* в качестве параметра - надо передать компаратор(сравниватель (byId))
+    after.stream().max(byId) - нашли max элемент, далее
+* получить этот max элемент get()
+* и взять у этого max элемента - id - getId()
+* далее надо реализовать сравниватель (byId)
+
+    int max1 = after.stream().max(byId).get().getId();
+    group.setId(max1);
+
+
+// III ВАРИАНТ - Лямбда-выражения / анонимные функции
+
+
+ 1.
+* Comparator<? super GroupData> byId = new Comparator<GroupData>() - нажать Alt + Enter на new Comparator - Replace with lambda
+
+   Comparator<? super GroupData> byId = (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+
+* (o1, o2) - параметры
+* Integer.compare(o1.getId(), o2.getId()); - тело функции
+
+    int max1 = after.stream().max(byId).get().getId();
+
+ 2.
+* (o1, o2) -> Integer.compare(o1.getId(), o2.getId()) - эту анонимную фун-ию / лямбла-выражение, можно передать в качестве параметра byId
+
+    int max1 = after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
+
+*  список превращаем в поток - по этому потоку пробегает фун-ия-сравниватель и находит max елемент
+*                              при этом сравниваются объекты типа GroupData, путем сравнения их id
+*                                            на ВЫХОДЕ функции - будет max объект(группа с max id)
+*                                                           остается только взять группу с max id
+
+** I ВАРИАНТ - Цикл можно убрать
+* от max1 - можно избавиться
+*   int max1 =
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+  }
+
+*//*---------------------TOTAL--------------------------------------------*/
+
+     @Test
+
+     public void testGroupCreation() throws Exception {
+
+    app.getNavigationHelper().gotoGroupPage();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    GroupData group = new GroupData("test4", "test_test", "test_test_test");
+    app.getGroupHelper().createGroup(group);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+
+    Assert.assertEquals(after.size(), before.size() + 1);
+
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+}
 
 }
 
