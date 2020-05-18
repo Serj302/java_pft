644609071e1664095fe1dedefec50453bbc96561
Serj302 <MarS302@mail.ru>
2020-05-18 18:08:11 +0300

    Актуализация кода

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -6,12 +6,14 @@ import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.List;
 
+/*
 public class GroupDeletionTests extends TestBase {
 
   @Test
     public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
-/* Проверка и обеспечение выполнения предусловий.
+
+// ПРОВЕРКА И ОБЕСПЕЧЕНИЕ ВЫПОЛНЕНИЯ ПРЕДУСЛОВИЙ Проверка и обеспечение выполнения предусловий.
 
 *  Проверка на наличие группы. Если группы нет - создать.
 
@@ -25,55 +27,72 @@ public class GroupDeletionTests extends TestBase {
                   app.getGroupHelper().returnToGroupPage();
 
 *  НО... много кода + дублирования, необходимо объеденить все эти шаги в вспомогательный метод createGroup
-
       app.getGroupHelper().createGroup (new GroupData("test0", null, null));
-*/
+//
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup (new GroupData("test0", null, null));
     }
 
-// контроль кол-ва элементов
+// КОНТРОЛЬ КОЛ-ВА ЭЛЕМЕНТОВ
 
-// обращать внимание с какой страницы начинается подсчет
-//  int before = app.getGroupHelper().getGroupCount();              // кол-во групп ДО добавления
+* обращать внимание с какой страницы начинается подсчет
+   int before = app.getGroupHelper().getGroupCount();              // кол-во групп ДО добавления
 
 // СРАВНЕНИЕ РАЗМЕРА списков before и after
+// МЕНЯЕМ КОЛЛИЧЕСТВО групп на СПИСОК групп
 
-// меняем КОЛЛИЧЕСТВО групп на СПИСОК групп
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().selectGroup(before.size() - 1);      // выбор последней группы, если надо выбрать НЕ последний элемент, а 2й - ...selectGroup(1);
-// если нужно удалить 1 группу передаем 0(selectGroup(0)),
-// если последнюю - before - 1, т.к. индекс последнего элемента -1
+    app.getGroupHelper().selectGroup(before.size() - 1);
+* выбор последней группы, если надо выбрать НЕ последний элемент, а 2й - ...selectGroup(1);
+* если нужно удалить 1 группу передаем 0(selectGroup(0)),
+* если последнюю - before - 1, т.к. индекс последнего элемента -1
+
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
-//  int after = app.getGroupHelper().getGroupCount();             // кол-во групп ПОСЛЕ добавления
-// меняем КОЛЛИЧЕСТВО групп на СПИСОК групп
+*  int after = app.getGroupHelper().getGroupCount();             // кол-во групп ПОСЛЕ добавления
+* меняем КОЛЛИЧЕСТВО групп на СПИСОК групп
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() - 1); //кол-во групп ПОСЛЕ - должно уменьшиться по отношению с кол-вом ДО - на 1
 
-/* СРАВНЕНИЕ списка групп (чтобы убедиться в том, что группа удалилась корректно )
+// СРАВНЕНИЕ СПИСКА ГРУПП (чтобы убедиться в том, что группа удалилась корректно )
 
 * before(старый) список - содержит на 1 элемент больше, чем after(новый)
 * чтобы сравнить списки - они должны быть равными, для этого надо удалить в старом списке лишний элемент
 * удалить лишний элемент, с тем же индексом
-*/
+*
     before.remove(before.size() - 1);
-// теперь before/старый список - содрежит те же элементы, что и after/новый список
+* теперь before/старый список - содрежит те же элементы, что и after/новый список
 
-/* проверка совпадений списка
+// ПРОВЕРКА СОВПАДЕНИЙ СПИСКА
     for (int i = 0; i < after.size(); i++){
       Assert.assertEquals(before.get(i), after.get(i));  // сравнение 2х элементов с одинаковыми индексами
     }
-*/
+
 // можно проверить совпадение и без цикла
     Assert.assertEquals(before, after);
 
-
-
   }
 
+*/
 
+ public class GroupDeletionTests extends TestBase {
 
+  @Test
+  public void testGroupDeletion() throws Exception {
+    app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup (new GroupData("test0", null, null));
+    }
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() - 1);
+    app.getGroupHelper().deleteSelectedGroups();
+    app.getGroupHelper().returnToGroupPage();
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() - 1);
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after);
+
+  }
 
 }
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -9,61 +9,94 @@ import java.util.List;
 
 public class GroupModificationTests extends TestBase{
 
+/*
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
-//Проверка на наличие группы. Если группы нет - создает.
+//ПРОВЕРКА НА НАЛИЧИЕ ГРУППЫ. Если группы нет - создает.
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup (new GroupData("test0", null, null));
-/* без Доп конструктора
-*     app.getGroupHelper().createGroup (new GroupData(null, "test0", null, null));
-*/
-
+* БЕЗ ДОП КОНСТРУКТОРА
+     app.getGroupHelper().createGroup (new GroupData(null, "test0", null, null));
+*
     }
-// контроль кол-ва элементов
-// обращать внимание с какой страницы начинается подсчет
-//  int before = app.getGroupHelper().getGroupCount();              // кол-во групп ДО добавления
-// меняем КОЛЛИЧЕСТВО групп на СПИСОК групп
+
+// КОНТРОЛЬ КОЛ-ВА ЭЛЕМЕНТОВ
+
+* обращать внимание с какой страницы начинается подсчет
+   int before = app.getGroupHelper().getGroupCount();              // кол-во групп ДО добавления
+
+// ВМЕСТО КОЛЛИЧЕСТВА ГРУПП - СПИСОК
+
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() -1);      // если надо выбрать НЕ последний элемент, а 2й - ...selectGroup(1);
     app.getGroupHelper().initGroupModification();
-/*  app.getGroupHelper().fillGroupForm(new GroupData("test0", "test33", "test44"));
-*   создать докальную переменную group*
-*/
+    app.getGroupHelper().fillGroupForm(new GroupData("test0", "test33", "test44"));
+*   создать Локальную переменную group*
 
-/* без Доп конструктора
-*     GroupData group = new GroupData(null, "test0", null, null);
-*/
+*** БЕЗ ДОП КОНСТРУКТОРА ***
+     GroupData group = new GroupData(null, "test0", null, null);
 
-/*    GroupData group = new GroupData("test1", "test33", "test44");
+
+    GroupData group = new GroupData("test1", "test33", "test44");
 * При модификации группы, передаются новые: name, header, footer, а старый id - необходимо сохранять
-*/
+
     GroupData group = new GroupData(before.get(before.size() -1).getId(), "test1", "test33", "test44");
 
     app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
-//  int after = app.getGroupHelper().getGroupCount();             // кол-во групп ПОСЛЕ добавления
-//  меняем КОЛЛИЧЕСТВО групп на СПИСОК групп
+
+    int after = app.getGroupHelper().getGroupCount();             // кол-во групп ПОСЛЕ добавления
+
+*  меняем КОЛЛИЧЕСТВО групп на СПИСОК групп
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());             //кол-во групп ПОСЛЕ - должно совпасть с кол-вом ДО
 
-/* СРАВНЕНИЕ списка групп (чтобы убедиться в том, что группа поменялась корректно)
+// СРАВНЕНИЕ СПИСКА ГРУПП (чтобы убедиться в том, что группа поменялась корректно)
+
 * есть проблема: при изменении группы - меняется и ее имя, из-за этого она может поменять своё место в списке
 * т.к. в тестовом приложении - группы сортируются по именам и т.к. порядок элементов в списке меняется - это создает сложности при сравнении 2ух списков
 * как решить?
 * сравнивать без учета порядка, главное, чтобы все элементы присутсвовали, порядок не важен
 * в Java есть: упорядоченные коллекции - списки и НЕ упорядоченные коллекции - множества
 * нужно перед сравнением, преобразовать эти списки в множества и сравнивать получившиеся множества
- */
 
-//модифицировать старый список, удалить последний элемент
+* модифицировать старый список, удалить последний элемент
     before.remove(before.size() - 1);
 
-// и добавить тот, который должен появиться после модификации
+* и добавить тот, который должен появиться после модификации
     before.add(group);                  // добавить в before new GroupData("test0", "test33", "test44")
 
-// преобразование списка в множества
+* преобразование списка в множества
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
+  }
+
+*/
+
+  @Test
+  public void testGroupModification() {
+
+    app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+    app.getGroupHelper().createGroup (new GroupData("test0", null, null));
+    }
+
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() -1);
+    app.getGroupHelper().initGroupModification();
+    GroupData group = new GroupData(before.get(before.size() -1).getId(), "test1", "test33", "test44");
+    app.getGroupHelper().fillGroupForm(group);
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() - 1);
+
+    before.add(group);
+
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
   }
