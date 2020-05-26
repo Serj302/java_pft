0581b6b29115513818920083a321887b2b238b15
Serj302 <MarS302@mail.ru>
2020-05-26 12:46:21 +0300

    5.4. Fluent-интерфейсы_ContactDeletion

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -8,34 +8,16 @@ import ru.stqa.pft.addressbook.model.ContactData;
 import java.util.List;
 
 public class ContactDeletionTests extends TestBase{
+
 // Перед каждым тестом - проверка предусловия (Актуализация кода)
   @BeforeMethod
   public void ensurePreconditionsDel() {
+    app.goTo().сontactPage();
   // Проверка на наличие контакта. Если контакта нет - создать. (По аналогии testGroupDeletion)
     if (app.contacts().list().size() == 0){
       app.goTo().сontactPage();
-      ContactData contact = new ContactData (
-                    "Сидор",
-                    "Petrovich3",
-                    "Сидоров",
-                    "IPI",
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    "[none]",
-                    null,
-                    null,
-                    null);
+      ContactData contact = new ContactData()
+              .withFirstname ("Сидор").withMiddlename("Petrovich3").withLastname("Сидоров").withNickname("IPI").withGroup("[none]");
     app.contacts().fill(contact,true);
     app.contacts().submit();
     app.goTo().homePage();
@@ -46,22 +28,13 @@ public class ContactDeletionTests extends TestBase{
     public void testContactDeletion(){
 
       List<ContactData> before = app.contacts().list();  // before - содержит список элементов/ before - содержит список объектов типа ContactData
-      int index = 0;
+      int index = before.size() - 1;
       app.contacts().deletion(index);
-      app.goTo().homePage();       // нужен переход на "home" c 3 sec time-out, т.к. идет задержка после удаления
+      app.goTo().homePage();                            // нужен переход на "home" c 3 sec time-out, т.к. идет задержка после удаления
       List<ContactData> after = app.contacts().list();  // меняем КОЛЛИЧЕСТВО контактов на СПИСОК контактов
       Assert.assertEquals(after.size(), before.size() - 1);
 
-//СРАВНЕНИЕ СПИСКОВ
-
-// удалить лишний элемент, С ТЕМ ЖЕ ИНДЕКСОМ, С КОТОРЫМ УДАЛЯЛИ (стр. 47)
-      before.remove(index); // теперь before/старый список - содрежит те же элементы, что и after/новый список
-/* ПРОВЕРКА СОВПАДЕНИЙ СПИСКА
-      for (int i = 0; i < after.size(); i++){
-        Assert.assertEquals(before.get(i), after.get(i));  // сравнение 2х элементов с одинаковыми индексами
-      }
-// можно проверить совпадение и без цикла
-*/
+      before.remove(index);                             // теперь before/старый список - содрежит те же элементы, что и after/новый список
       Assert.assertEquals(before, after);
 
     }
